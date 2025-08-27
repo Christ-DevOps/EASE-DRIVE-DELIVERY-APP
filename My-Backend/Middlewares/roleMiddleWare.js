329de04ee96fc206b97fb2758387265690b3a980
Christ-DevOps <tangomochrist@gmail.com>
2025-08-27 17:41:29 +0200

    Replace all files with only front-end and back-end folders

--- My-Backend/Middlewares/roleMiddleWare.js
+++
@@ -1,8 +0,0 @@
-// middleware/roleMiddleware.js
-const authorize = (...roles) => (req, res, next) => {
-  if (!req.user) return res.status(401).json({ message: 'Not authorized' });
-  if (!roles.includes(req.user.role)) return res.status(403).json({ message: 'Forbidden: insufficient role' });
-  next();
-};
-
-module.exports = authorize;
--- src/navigation/types.ts
+++
@@ -1,6 +0,0 @@
-export type RootStackParamList = {
-  Home: undefined;
-  Cart: undefined;
-  OrderConfirmation: { orderTotal: string };
-  // Add other screens as needed
-};
